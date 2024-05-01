a1fbd214e9cd69f6bf84231b34e8beb0f8d96408
ADV505 <dmitriy-505@mail.ru>
2024-05-01 17:54:28 +0500

    add class

---
+++ LessonOop/Calculator.cs
@@ -0,0 +1,58 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOop
+{
+    class CalculatorEventArgs : EventArgs
+    {
+        public double answer;
+    }
+
+    class Calculator
+    {
+        public event EventHandler<CalculatorEventArgs> Result;
+        private double result = 0;
+        Stack<double> results = new Stack<double>();
+
+        private void StartEvent()
+        {
+            Result?.Invoke(this, new CalculatorEventArgs { answer = result });
+        }
+
+        public void Add(double value)
+        {
+            results.Push(result);
+            result += value;
+            StartEvent();
+        }
+        public void Sub(double value)
+        {
+            results.Push(result);
+            result -= value;
+            StartEvent();
+        }
+        public void Mul(double value)
+        {
+            results.Push(result);
+            result *= value;
+            StartEvent();
+        }
+        public void Div(double value)
+        {
+            results.Push(result);
+            result /= value;
+            StartEvent();
+        }
+        public void Cancel()
+        {
+            if (results.Count > 0)
+            {
+                result = results.Pop();
+                StartEvent();
+            }
+        }
+    }
+}
