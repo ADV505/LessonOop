04b80d2225059639de96c73779f630aba322357f
ADV505 <dmitriy-505@mail.ru>
2024-05-05 00:29:36 +0500

    add Lesson7

---
+++ LessonOop/Lesson7.cs
@@ -0,0 +1,13 @@
+﻿public class Lesson7
+{
+    public int I { get; set; }
+    public string S { get; set; }
+    public decimal D { get; set; }
+    public char C { get; set; }
+
+    public Lesson7() { }
+    public Lesson7(int i) { this.I = i; }
+    public Lesson7(int i, string s, decimal d, char c) { this.I = i; this.S = s; this.D = d; this.C = c; }
+
+
+}
