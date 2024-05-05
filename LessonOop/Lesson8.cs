5bab8efdfac8a1d236c9655da82bff607b8cd0bf
ADV505 <dmitriy-505@mail.ru>
2024-05-05 21:48:44 +0500

    add Lesson8

---
+++ LessonOop/Lesson8.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOop
+{
+    public class Lesson8
+    {
+        public static string[] FindFile(string path, string fileFormat)
+        {              
+            return Directory.GetFiles(path, fileFormat, SearchOption.AllDirectories); 
+        }
+        public static List<string> ReaderFile(string[] strings, string text)
+        {
+            List<string> result = new List<string>();
+
+            for (int i = 0; i < strings.Length; i++)
+            {
+                string txt = File.ReadAllText(strings[i]);
+
+                if (txt.ToLower().Contains(text.ToLower()))
+                    result.Add(strings[i]);
+                    //Console.WriteLine($"Слово ({text}) есть в файле {strings[i]}" );
+            }
+
+            return result;
+        }
+    }
+}
