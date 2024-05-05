bb93b3cd6ba41adb9d613b2ce7af3e34bda7c886
ADV505 <dmitriy-505@mail.ru>
2024-05-05 15:16:36 +0500

    add class

---
+++ LessonOop/Bits.cs
@@ -0,0 +1,19 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LessonOop
+{
+    class Bits
+    {
+        public double Value { get; private set; } = 0;
+       
+
+        public static implicit operator Bits(byte b) {  return new Bits { Value = b}; }
+        public static implicit operator Bits(long l) { return new Bits { Value = l }; }
+        public static implicit operator Bits(int i) { return new Bits { Value = i }; }
+        
+    }
+}
