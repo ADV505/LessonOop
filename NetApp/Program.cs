b00d67825c1a6dae3ad7084a315df39933807d54
ADV505 <dmitriy-505@mail.ru>
2024-06-16 21:44:30 +0500

    Net

---
+++ NetApp/Program.cs
@@ -0,0 +1,48 @@
+﻿using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetApp
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+
+            Server();
+        }
+
+        public static void Server()
+        {
+            UdpClient udpClient = new UdpClient(12345);
+            IPEndPoint iPEndPoint = new IPEndPoint(IPAddress.Any, 0);
+
+            Console.WriteLine("Сервер ждет сообщение от клиента...");
+
+            while (true)
+            {
+                byte[] buffer = udpClient.Receive(ref iPEndPoint);
+                var messageText = Encoding.UTF8.GetString(buffer);
+
+                ThreadPool.QueueUserWorkItem(obj =>
+                {
+                    Message message = Message.DeserializeFromJson(messageText);
+                    message.Print();
+                    if (message.Text.ToString().ToLower() == "exit")
+                    {
+                        byte[] answer = Encoding.UTF8.GetBytes("Сервер ушел в аут");
+                        udpClient.Send(answer,answer.Length,iPEndPoint);
+
+                        Environment.Exit(0);
+                    }
+                    else
+                    {
+                        byte[] answer = Encoding.UTF8.GetBytes("Сообщение доставлено");
+                        udpClient.Send(answer, answer.Length, iPEndPoint);
+                    }
+
+                });
+            }
+        }
+    }
+}
---
+++ NetAppClient/Program.cs
@@ -0,0 +1,55 @@
+﻿using NetApp;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace NetAppClient
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {       
+            //for (int i = 0; i < 10; i++)
+            //{
+            SendMessage("Artem");
+
+            //}
+        }
+
+        public static void SendMessage(string from, string ip="127.0.0.1")
+        {
+            
+
+            UdpClient udpClient = new UdpClient();
+            IPEndPoint iPEndPoin = new IPEndPoint(IPAddress.Parse(ip), 12345);
+            string answer = "Сообщение доставлено";
+
+            
+            while (answer == "Сообщение доставлено")
+            {
+                string messageText;
+                do
+                {
+                    //Console.Clear();
+                    Console.Write("Введите сообщение: ");
+                    messageText = Console.ReadLine();
+                }
+
+                while (string.IsNullOrEmpty(messageText));
+                Message message = new Message() { DateTime = DateTime.Now, NicknameFrom = from, NicknameTo = "Anna", Text = messageText };
+                string json = message.SerializeMessegeToJson();
+
+                byte[] data = Encoding.UTF8.GetBytes(json);
+                udpClient.Send(data,data.Length, iPEndPoin);
+
+                
+                byte[] buffer = udpClient.Receive(ref iPEndPoin);
+                answer = Encoding.UTF8.GetString(buffer);
+               
+                    Console.WriteLine(answer);               
+            }
+            Console.WriteLine("Сервер завершил свою работу. Нажмите любую клавишу для завершения работы клиента");
+            Console.ReadKey();
+        }
+    }
+}
