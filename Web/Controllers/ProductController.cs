12a794729e8b5eb2bf263b7249180fad61f04333
ADV505 <dmitriy-505@mail.ru>
2024-06-25 23:10:22 +0500

    WebApp

---
+++ Web/Controllers/ProductController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> AddProduct(string name, string description, decimal price)
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                if (appContex.Products.Any(p => p.Name == name))
+                    return StatusCode(409);
+
+                var product = new Product() { Name = name, Description = description, Price = price };
+                appContex.Products.Add(product);
+                appContex.SaveChanges();
+                return Ok(product.Id);
+            }
+        }
+
+        [HttpGet("all")]
+        public ActionResult<IEnumerable<Product>> GetProducts()
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                var list = appContex.Products.Select(p => new Product { Id = p.Id, Name = p.Name, Description = p.Description, Price = p.Price }).ToList();
+                return Ok(list);
+                
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteProduct(int id)
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                Product? product = appContex.Products.FirstOrDefault(p => p.Id == id);
+                if (product == null)
+                    return StatusCode(409);
+                appContex.Products.Remove(product);
+                appContex.SaveChanges();
+                return Ok();
+            }
+            
+        }
+    }
+}
---
+++ Web/Controllers/ProductGroupController.cs
@@ -0,0 +1,53 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Web.Data;
+using Web.Models;
+
+namespace Web.Controllers
+{
+    [ApiController]
+    [Route("[controller]")]
+    public class ProductGroupController : ControllerBase
+    {
+        [HttpPost]
+        public ActionResult<int> AddProductGroup(string name, string description)
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                if (appContex.ProductGroups.Any(p => p.Name == name))
+                    return StatusCode(409);
+
+                var product = new Product() { Name = name, Description = description };
+                appContex.Products.Add(product);
+                appContex.SaveChanges();
+                return Ok(product.Id);
+            }
+        }
+
+        [HttpGet]
+        public ActionResult<IEnumerable<ProductGroup>> GetProductGroups()
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                var list = appContex.ProductGroups.Select(p => new Product { Id = p.Id, Name = p.Name, Description = p.Description}).ToList();
+                return Ok(list);
+
+            }
+        }
+
+        [HttpDelete]
+        public ActionResult DeleteProduct(int id)
+        {
+            using (AppContex appContex = new AppContex())
+            {
+                ProductGroup? productGroup = appContex.ProductGroups.FirstOrDefault(p => p.Id == id);
+                if (productGroup == null)
+                    return StatusCode(409);
+                appContex.ProductGroups.Remove(productGroup);
+                appContex.SaveChanges();
+                return Ok();
+            }
+
+        }
+
+    }
+}
